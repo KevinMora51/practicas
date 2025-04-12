21972e07f85e5cb0c50c38b6cf4ff3b48be02910
KevinMora51 <A24310216@live.ceti.mx>
2025-04-12 01:34:17 +0000

    practica 5

--- makefile
+++ makefile
@@ -14,3 +14,9 @@ bin/archivo : src/archivo.cpp
 
 archivo : bin/archivo 
 	./bin/archivo
+
+bin/binario : src/binario.cpp 
+	g++ src/binario.cpp -o bin/binario
+
+binario : bin/binario 
+	./bin/binario
--- src/binario.cpp
+++ src/binario.cpp
@@ -2,27 +2,38 @@
 #include<fstream>
 #include<string>
 using namespace std;
+
+struct Persona
+{
+    char nombre[50];
+    int edad;
+};
+
+
+
 int main(int argc, char const *argv[])
 {
    
     ofstream archivoSalida;
 
     // abrir el archivo salida
-    archivoSalida.open("archivo_salida.txt");
+    archivoSalida.open("archivo_salida.bin", ios::binary);
     if(!archivoSalida){
         cerr<< "Error al abrir el archivo de salida"<<endl;
         return 1;
 
     
     }
-
+    Persona p1 = {"juan",30};
+    Persona p2 = {"Maria",25};
     //escribir el archivo de salida 
-    archivoSalida <<"Hola, este es un archivo de salid. "<<endl;
-
+    archivoSalida.write((const char*)(&p1),sizeof(Persona));
+    archivoSalida.write((const char*)(&p2),sizeof(Persona));
     //cerrar archivo de salida
     archivoSalida.close();
 
-    ifstream archivoEntrada;
+
+   /* ifstream archivoEntrada;
     string linea;
 
     // abrir el archivo salida
@@ -41,12 +52,7 @@ int main(int argc, char const *argv[])
    
 
     //cerrar archivo de salida
-    archivoSalida.close();
-
-    
-
-
-
+    archivoSalida.close();*/
 
 
 
